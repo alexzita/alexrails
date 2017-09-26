748c3a2a329ac0ac5fb9b797e8e984fdd0103219
alexzita <alexzita5@gmail.com>
2017-09-26 12:44:15 -0400

    added admin as final touch

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@
 class UsersController < ApplicationController
 before_action :set_user, only: [:edit, :update, :show]
 before_action :require_same_user, only: [:edit, :update]
+before_action :require_admin, only: [:destroy]
 
 
 def new
@@ -11,8 +12,9 @@ end
 def create
  @user = User.new(user_params)
  if @user.save
+    session[:user_id] = @user.id
     flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
-    redirect_to articles_path
+    redirect_to user_path(@user)
  else
     render 'new'
  end
@@ -59,6 +61,18 @@ end
 
 end
 
+def destroy
+
+@user = User.find(params[:id])
+
+@user.destroy
+
+flash[:danger] = "User and all articles created by user have been deleted"
+
+redirect_to users_path
+
+end
+
 private
   def user_params
     params.require(:user).permit(:username, :email, :password)
@@ -69,6 +83,12 @@ private
   def require_same_user
      if current_user != @user
      flash[:danger] = "You can only edit your own account"
+     redirect_to root_path
+     end
+  end
+  def require_admin
+     if logged_in? and !current_user.admin?
+     flash[:danger] = "Only admin users can perform that action"
      redirect_to root_path
      end
   end
