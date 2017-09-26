d0574272bb6158da2b84a24fd3ca133b0ab9f18f
alexzita <alexzita5@gmail.com>
2017-09-25 22:37:29 -0400

    Final version

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ get '/static_pages/about', to: 'static_pages#about'
 resources  :articles
 get 'signup', to:'users#new'
 resources :users, except: [:new]
+get 'login', to:'sessions#new'
+post 'login', to: 'sessions#create'
+delete 'logout', to: 'sessions#destroy'
 end
